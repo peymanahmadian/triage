3be7a25c151be0d1d2d7f44f2126632b985cb2f8
peyman ahmadian <ahmadian.peyman@gmail.com>
2021-02-08 02:52:49 +0330

    setup triage

--- src/view/triage/triage.jsx
+++ src/view/triage/triage.jsx
@@ -1,4 +1,4 @@
-import {Layout, Row,Col} from "antd";
+import {Layout, Row, Col, Radio, Checkbox,Slider,Input} from "antd";
 import "../../assets/styles/triage.scss";
 import SidebarTriage from "./sidebarTriage";
 import {LeftOutlined,HeartOutlined,ColumnWidthOutlined} from "@ant-design/icons";
@@ -13,6 +13,37 @@ const Triage=(props)=>{
                     <Col xs={24} md={24}>
                         <fieldset>
                             <legend className={"red"}><LeftOutlined /><span>سطح یک</span></legend>
+                            <div className={"grayTitle"}>
+                                <div className={"grayHead"}>سطح هوشیاری بیمار</div>
+                                <div className={"grayContent"}>
+                                    <Radio.Group onChange={()=>{}} value={1}>
+                                        <Radio value={1}>A</Radio>
+                                        <Radio value={2}>V</Radio>
+                                        <Radio value={3}>P</Radio>
+                                        <Radio value={4}>U</Radio>
+                                    </Radio.Group>
+                                </div>
+                            </div>
+                            <Row className={"container"}>
+                                <Col xs={24} sm={12} md={4}>
+                                    <Checkbox>مخاطره راه هوایی</Checkbox>
+                                </Col>
+                                <Col xs={24} sm={12} md={4}>
+                                    <Checkbox>دیسترس تنفسی</Checkbox>
+                                </Col>
+                                <Col xs={24} sm={12} md={4}>
+                                    <Checkbox>سیانوز</Checkbox>
+                                </Col>
+                                <Col xs={24} sm={12} md={4}>
+                                    <Checkbox>علائم شوک</Checkbox>
+                                </Col>
+                                <Col xs={24} sm={12} md={4}>
+                                    <Checkbox>SPO</Checkbox>
+                                </Col>
+                                <Col xs={24} sm={12} md={4}>
+                                    <Checkbox>انتوباسیون</Checkbox>
+                                </Col>
+                            </Row>
                         </fieldset>
                     </Col>
                 </Row>
@@ -20,6 +51,17 @@ const Triage=(props)=>{
                     <Col xs={24} md={24}>
                         <fieldset>
                             <legend className={"yellow"}><LeftOutlined /><span>سطح دوم</span></legend>
+                            <div className={"grayTitle"}>
+                                <div className={"grayHead"}>شرایط</div>
+                                <div className={"grayContent"}>
+                                    <Radio.Group onChange={()=>{}} value={1}>
+                                        <Radio value={1}>پر خطر</Radio>
+                                        <Radio value={2}>لتاژی و خواب آلودگی</Radio>
+                                        <Radio value={3}>دیسترس شدید</Radio>
+                                    </Radio.Group>
+                                </div>
+                            </div>
+
                         </fieldset>
                     </Col>
                 </Row>
@@ -27,6 +69,45 @@ const Triage=(props)=>{
                     <Col xs={24} md={24}>
                         <fieldset>
                             <legend className={"gray"}><LeftOutlined /><span>سطوح بالاتر</span></legend>
+                            <Row gutter={[12,12]}>
+                                <Col xs={24} md={12}>
+                                    <div className={"toolbar"}>
+                                            <div className={"label"}>میزان درد</div>
+                                            <Slider className={"bar"} defaultValue={30}  />
+                                    </div>
+
+                                </Col>
+                                <Col xs={24} md={12}>
+                                    <div className={"toolbar"}>
+                                        <Input
+                                            prefix={<span className={"grayText"}> میزان قند خون:</span>}
+                                        />
+                                    </div>
+                                </Col>
+                            </Row>
+                            <Row gutter={[12,12]}>
+                                <Col xs={24} md={12}>
+                                    <label className={"paddingBottom"}>سابقه حساسیت دارویی</label>
+                                    <Input.Search
+                                        allowClear
+                                        enterButton="لیست دارو"
+                                        size="medium"
+                                        onSearch={()=>{}}
+                                    />
+                                </Col>
+                                <Col xs={24} md={12}>
+                                    <label className={"paddingBottom"}>سابقه حساسیت دارویی</label>
+                                    <Input/>
+                                </Col>
+                            </Row>
+                            <Row gutter={[12,6]}>
+                                <Col xs={24} md={12}>
+                                    <Input.TextArea rows={4}/>
+                                </Col>
+                                <Col xs={24} md={12}>
+                                    <Input.TextArea rows={4}/>
+                                </Col>
+                            </Row>
                         </fieldset>
                     </Col>
                 </Row>
