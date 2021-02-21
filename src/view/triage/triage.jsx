cf9bdcf2b92e542d37f80a853fc67917b1149ebb
peyman ahmadian <ahmadian.peyman@gmail.com>
2021-02-21 23:39:49 +0330

    setup complete UI

--- src/view/triage/triage.jsx
+++ src/view/triage/triage.jsx
@@ -1,6 +1,12 @@
-import {Layout, Row, Col, Radio, Checkbox,Slider,Input} from "antd";
+import {Layout, Row, Col, Radio, Checkbox, Slider, Input, Select,Upload} from "antd";
+import {UploadOutlined} from "@ant-design/icons";
 import "../../assets/styles/triage.scss";
 import SidebarTriage from "./sidebarTriage";
+import account from "./../../assets/images/icons/AccountInfo.svg";
+import general from "./../../assets/images/icons/general.svg";
+import complain from "./../../assets/images/icons/complain.svg";
+import drug from "./../../assets/images/icons/drug.svg";
+
 import {LeftOutlined,HeartOutlined,ColumnWidthOutlined} from "@ant-design/icons";
 import {Button,Combo} from "../../components";
 const Triage=(props)=>{
@@ -10,6 +16,179 @@ const Triage=(props)=>{
                 <SidebarTriage/>
             </Layout.Sider>
             <Layout.Content className={"content"}>
+                <Row className={"headerTriage"} gutter={[4,4]}>
+                    <Col xs={24} md={6}>
+                        <div className={"showText"}><span></span><b>کدتریاژ</b></div>
+                    </Col>
+                    <Col xs={24} md={6}>
+                        <div className={"showText"}><span></span><b>نام بیمار</b></div>
+                    </Col>
+                    <Col xs={24} md={6}>
+                        <div className={"showText"}><span></span><b>سطح تریاژ</b></div>
+                    </Col>
+                    <Col xs={24} md={6}>
+                        <div className={"showText"}><span></span><b>ارجاع به</b></div>
+                    </Col>
+                    <Col xs={24} md={6}>
+                        <div className={"showText"}><span></span><b>تاریخ مراجعه</b></div>
+                    </Col>
+                    <Col xs={24} md={6}>
+                        <div className={"showText"}><span></span><b>علت مراجعه</b></div>
+                    </Col>
+                    <Col xs={24} md={6}>
+                        <div className={"showText"}><span></span><b>نحوه ورود</b></div>
+                    </Col>
+                    <Col xs={24} md={6}>
+                        <div className={"showText"}><span></span><b>تریاژ کننده</b></div>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col xs={24} md={24}>
+                        <fieldset>
+                            <legend>
+                                <img src={account} height={32} alt={"اطلاعات فردی"}/><div>اطلاعات فردی</div>
+                            </legend>
+                            <Row gutter={[6,4]}>
+                                <Col xs={12} md={3} className={"paddingTop"}>
+                                    <Checkbox>فاقد هویت</Checkbox>
+                                </Col>
+                                <Col xs={12} md={3}>
+                                    <Select size={"large"} className={"full"} placeholder={"جنسیت"}>
+                                        <Select.Option value={1}>مرد</Select.Option>
+                                        <Select.Option value={2}>زن</Select.Option>
+                                    </Select>
+                                </Col>
+                                <Col xs={24} md={5}>
+                                    <Input size={"large"} placeholder={"نام"}/>
+                                </Col>
+                                <Col xs={24} md={6}>
+                                    <Input size={"large"} placeholder={"نام خانوادگی"}/>
+                                </Col>
+                                <Col xs={24} md={2}>
+                                    <Input type={"number"} size={"large"} placeholder={"سن"}/>
+                                </Col>
+                                <Col xs={24} md={5}>
+                                    <Input size={"large"} placeholder={"کد ملی"}/>
+                                </Col>
+                            </Row>
+                            <Row gutter={[6,4]}>
+                                <Col xs={12} md={6}>
+                                    <Upload block>
+                                        <Button><UploadOutlined />  بارگذاری تصویر  </Button>
+                                    </Upload>
+                                </Col>
+                                <Col xs={24} md={5}>
+                                    <Input size={"large"} placeholder={"تلفن"}/>
+                                </Col>
+                                <Col xs={24} md={6}>
+                                    <Input size={"large"} placeholder={"نام پدر"}/>
+                                </Col>
+                                <Col xs={24} md={2}>
+                                    <Input type={"number"} size={"large"} placeholder={"وزن"}/>
+                                </Col>
+                                <Col xs={24} md={5}>
+                                    <Input size={"large"} placeholder={"شغل"}/>
+                                </Col>
+                            </Row>
+                            <Row gutter={[6,4]}>
+                                <Col xs={24} md={17}>
+                                    <Input type={"text"} size={"large"} placeholder={"آدرس"}/>
+                                </Col>
+                                <Col xs={24} md={2} className={"paddingTop"}>
+                                    <Checkbox>باردار</Checkbox>
+                                </Col>
+                                <Col xs={24} md={5}>
+                                    <Input size={"large"} placeholder={"تلفن"}/>
+                                </Col>
+                            </Row>
+                        </fieldset>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col xs={24} md={24}>
+                        <fieldset>
+                            <legend>
+                                <img src={general} height={32} alt={"عمومی"}/><div>عمومی</div>
+                            </legend>
+                            <Row gutter={[6,4]}>
+                                <Col xs={12} md={7}>
+                                    <Input size={"large"} placeholder={"علت مراجعه"}/>
+                                </Col>
+                                <Col xs={12} md={7}>
+                                    <Input size={"large"} placeholder={"نحوه ورود"}/>
+                                </Col>
+                                <Col xs={24} md={5}>
+                                    <Input size={"large"} placeholder={"نحوه مراجعه"}/>
+                                </Col>
+                                <Col xs={24} md={5}>
+                                    <Input size={"large"} placeholder={"کد آمبولانس"}/>
+                                </Col>
+                                <Col xs={24} md={16}>
+                                    <div className={"grayTitle"}>
+                                        <div className={"grayHead"}>مراجعه در 24 گذشته</div>
+                                        <div className={"grayContent"}>
+                                            <Radio.Group onChange={()=>{}} value={1}>
+                                                <Radio value={2}>ندارد</Radio>
+                                                <Radio value={3}>همین بیمارستان</Radio>
+                                                <Radio value={4}>بیمارستان</Radio>
+
+                                            </Radio.Group>
+                                        </div>
+
+                                    </div>
+                                </Col>
+                                <Col xs={24} md={8}>
+                                    <Input size={"large"} placeholder={"نام بیمارستان"}/>
+                                </Col>
+                            </Row>
+                            <Row gutter={[6,4]}>
+                                <Col xs={24} md={6}>
+                                    <Input size={"large"} placeholder={"ارجاع درمانگاه"}/>
+                                </Col>
+                                <Col xs={24} md={6}>
+                                    <Input size={"large"} placeholder={"از"}/>
+                                </Col>0
+                            </Row>
+                        </fieldset>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col xs={24} md={24}>
+                        <fieldset>
+                            <legend>
+                                <img src={complain} height={32} alt={"شکایت اصلی"}/><div>شکایت اصلی</div>
+                            </legend>
+                            <Row gutter={[6,4]}>
+                                <Col xs={24} md={12}>
+                                    <Input size={"large"} placeholder={"عنوان"}/>
+                                    <Input size={"large"} placeholder={"شرح"} className={"marginTop"}/>
+                                </Col>
+                                <Col xs={24} md={12}>
+                                    <Input.TextArea rows={3} size={"large"} placeholder={""}/>
+                                </Col>
+                            </Row>
+                        </fieldset>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col xs={24} md={24}>
+                        <fieldset>
+                            <legend>
+                                <img src={drug} height={32} alt={"سابقه حساسیت دارویی"}/><div>سابقه حساسیت دارویی</div>
+                            </legend>
+                            <Row gutter={[6,4]}>
+                                <Col xs={24} md={12}>
+                                    <Checkbox>سابقه حساسیت دارویی</Checkbox>
+                                    <Input size={"large"} placeholder={"نام دارو"} className={"marginTop"}/>
+                                    <Button color={"blue"} className={"marginTop"}>لیست داروها</Button>
+                                </Col>
+                                <Col xs={24} md={12}>
+                                    <Input.TextArea rows={4} size={"large"} placeholder={""}/>
+                                </Col>
+                            </Row>
+                        </fieldset>
+                    </Col>
+                </Row>
                 <Row>
                     <Col xs={24} md={24}>
                         <fieldset>
